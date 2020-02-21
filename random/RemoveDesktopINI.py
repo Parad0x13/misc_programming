2c4580eb8babbe33ea845b371ed093532d0da136
Bryan Tremblay <bryanandhallie@gmail.com>
2020-02-20 21:53:40 -0500

    RemoveDesktopINI.py is redundant as it exists in mist

--- random/RemoveDesktopINI.py
+++
@@ -1,9 +0,0 @@
-import os
-rootdir = "G:/Google Drive/"
-
-for subdir, dirs, files in os.walk(rootdir):
-	for file in files:
-		proper = os.path.join(subdir, file)
-		if file == "desktop.ini":
-			print(proper)
-			os.remove(proper)
